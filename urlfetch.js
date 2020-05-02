ba20119194046619377e8b2d1e36e493788575a9
Fumikazu Fujiwara | Freddie <github@ze.gs>
2020-05-03 08:41:43 +0900

    add args

--- urlfetch.js
+++ urlfetch.js
@@ -1,24 +1,30 @@
 class UrlFetch {
 
-  constructor() {}
+    constructor() {}
 
-  getInfo() { 
-    return {
-      id: 'urlfetch',
-      name: 'UrlFetch', 
-      blocks: [ 
-        {
-          opcode: 'fetch',
-          blockType: Scratch.BlockType.COMMAND,　
-          text: 'UrlFetch' 
+    getInfo() {
+        return {
+            id: 'urlfetch',
+            name: 'UrlFetch',
+            blocks: [{
+                opcode: 'fetch',
+                blockType: Scratch.BlockType.COMMAND,
+                text: 'UrlFetch',
+                arguments: {
+                    TEXT: {
+                        type: ArgumentType.STRING,
+                        defaultValue: "https://www.google.com/"
+                    }
+                }
+            }]
         }
-      ]
     }
-  }
 
-  fetch() {
-    console.log('fetch');
-  }
+    fetch(args) {
+        const text = Cast.toString(args.TEXT);
+        console.log(text);
+        log.log(text);
+    }
 }
 
 Scratch.extensions.register(new UrlFetch());
